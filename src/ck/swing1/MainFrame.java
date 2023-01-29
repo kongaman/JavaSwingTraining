999889650a1f21bd29a650775be66083a38f90ed
kongaman <christian.kranz@gmail.com>
2023-01-29 19:40:37 +0100

    Adding communiction
    between custonm components (Toolbar and TextPanel) in a suboptimal way

--- src/ck/swing1/MainFrame.java
+++ src/ck/swing1/MainFrame.java
@@ -2,14 +2,12 @@ package ck.swing1;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame{
 	
 	private Toolbar toolbar;
 	private TextPanel textPanel;
-	private JButton btn;
 
 	public MainFrame()  {
 		super("Hello World");
@@ -17,16 +15,11 @@ public class MainFrame extends JFrame{
 		
 		toolbar = new Toolbar();
 		textPanel = new TextPanel();
-		btn = new JButton("Click Me!");
 		
-		btn.addActionListener(e -> {
-			textPanel.appendText("Hello!\n");
-		});
+		toolbar.setTextPanel(textPanel);
 		
 		add(toolbar, BorderLayout.NORTH);
 		add(textPanel, BorderLayout.CENTER);
-		add(btn, BorderLayout.SOUTH);
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(600,500);
 		setVisible(true);
