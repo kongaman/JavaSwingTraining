e1f1a866d58600a4c2f9a5402af4864b54891d21
kongaman <christian.kranz@gmail.com>
2023-01-29 19:14:52 +0100

    Custom component TextPanel

--- src/ck/swing1/MainFrame.java
+++ src/ck/swing1/MainFrame.java
@@ -4,25 +4,24 @@ import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame{
 	
-	private JTextArea textArea;
+	private TextPanel textPanel;
 	private JButton btn;
 
 	public MainFrame()  {
 		super("Hello World");
 		setLayout(new BorderLayout());
 		
-		textArea = new JTextArea();
+		textPanel = new TextPanel();
 		btn = new JButton("Click Me!");
 		
 		btn.addActionListener(e -> {
-			textArea.append("Hello\n");
+			textPanel.appendText("Hello!\n");
 		});
 		
-		add(textArea, BorderLayout.CENTER);
+		add(textPanel, BorderLayout.CENTER);
 		add(btn, BorderLayout.SOUTH);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
