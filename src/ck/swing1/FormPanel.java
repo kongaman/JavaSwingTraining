e7a6298f8d8c88ffcd630728e7154edf05aed2a6
kongaman <christian.kranz@gmail.com>
2023-02-01 12:40:00 +0100

    Listbox implemented

--- src/ck/swing1/FormPanel.java
+++ src/ck/swing1/FormPanel.java
@@ -6,8 +6,10 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 
 import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
@@ -20,6 +22,7 @@ public class FormPanel extends JPanel {
 	private JTextField occupationField;
 	private JButton okBtn;
 	private FormListener formListener;
+	private JList<String> ageList;
 
 	public FormPanel() {
 		Dimension dim = getPreferredSize();
@@ -33,11 +36,23 @@ public class FormPanel extends JPanel {
 		occupationLabel = new JLabel("Occupation: ");
 		nameField = new JTextField(10);
 		occupationField = new JTextField(10);
+		ageList = new JList<>();
+		
+		DefaultListModel<String> ageModel = new DefaultListModel<>();
+		ageModel.addElement("Under 18");
+		ageModel.addElement("18 to 65");
+		ageModel.addElement("65 and over");
+		ageList.setModel(ageModel);
+		
+		ageList.setPreferredSize(new Dimension(115, 60));
+		ageList.setBorder(BorderFactory.createEtchedBorder());
+		ageList.setSelectedIndex(1);
 		
 		okBtn = new JButton("OK");
 		okBtn.addActionListener(e -> {
 			String name = nameField.getText();
 			String occupation = occupationField.getText();
+			String ageCat = ageList.getSelectedValue();
 			
 			FormEvent ev = new FormEvent(this, name, occupation);
 			if (formListener != null) {
@@ -78,9 +93,16 @@ public class FormPanel extends JPanel {
 		add(occupationField, gc);
 		
 		//Line 3
-		gc.weighty = 2; // y-axis weight higher so line 3 takes up all the remaining space below line 1 and 2
 		gc.gridx = 1;
 		gc.gridy = 2;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(ageList, gc);
+		
+		//Line 4
+		gc.weighty = 2; // y-axis weight higher so line 3 takes up all the remaining space below line 1 and 2
+		gc.gridx = 1;
+		gc.gridy = 3;
 		gc.anchor = GridBagConstraints.FIRST_LINE_START;
 		gc.insets = new Insets(0, 0, 0, 0);
 		add(okBtn, gc);
