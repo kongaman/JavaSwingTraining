135232181b18959d918646c16c711180f4236454
kongaman <christian.kranz@gmail.com>
2023-02-05 16:45:19 +0100

    Dialog added

---
+++ src/ck/swing1/gui/PrefsDialog.java
@@ -0,0 +1,14 @@
+package ck.swing1.gui;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class PrefsDialog extends JDialog {
+	
+	public PrefsDialog(JFrame parent) {
+		super(parent, "Preferences", false);
+		
+		setSize(400, 300);
+	}
+
+}
