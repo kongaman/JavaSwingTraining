70abecff0da498262cb3d2ad47292e4c73f31551
kongaman <christian.kranz@gmail.com>
2023-02-06 07:30:19 +0100

    Added spinner to select portnumber

--- src/ck/swing1/gui/PrefsDialog.java
+++ src/ck/swing1/gui/PrefsDialog.java
@@ -1,14 +1,67 @@
 package ck.swing1.gui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 
 public class PrefsDialog extends JDialog {
+	
+	private JButton okButton;
+	private JButton cancelButton;
+	private JSpinner portSpinner;
+	private SpinnerNumberModel spinnerModel;
 	
 	public PrefsDialog(JFrame parent) {
 		super(parent, "Preferences", false);
 		
 		setSize(400, 300);
+		setLocationRelativeTo(parent);
+		
+		okButton = new JButton("OK");
+		cancelButton = new JButton("Cancel");
+		
+		spinnerModel = new SpinnerNumberModel(3306, 0, 9999, 1);
+		portSpinner = new JSpinner(spinnerModel);
+		
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints gc = new GridBagConstraints();
+		
+		gc.weightx = 1;
+		gc.weighty = 1;
+		gc.fill = GridBagConstraints.NONE;
+		
+		//Row
+		gc.gridy = 0;
+		gc.gridx = 0;
+		add(new JLabel("Port: "), gc);
+		gc.gridx++;
+		add(portSpinner, gc);
+		
+		//Row
+		gc.gridy++;
+		gc.gridx = 0;
+		add(okButton, gc);
+		gc.gridx++;
+		add(cancelButton, gc);
+		
+		okButton.addActionListener(e -> {
+			int value = (int) portSpinner.getValue();
+			System.out.println(value);
+			
+			setVisible(false);
+		});
+		
+		cancelButton.addActionListener(e -> {
+			
+			setVisible(false);
+		});
 	}
 
 }
